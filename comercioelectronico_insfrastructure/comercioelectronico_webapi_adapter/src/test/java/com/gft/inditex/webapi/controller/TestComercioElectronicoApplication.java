b9972f2ab86a7d51315cc3761ac04e689feccb54
belbedoui <bilal.elbedoui@soprasteria.com>
2024-08-17 13:15:46 +0200

    refacto test adapterweb

--- comercioelectronico_insfrastructure/comercioelectronico_webapi_adapter/src/test/java/com/gft/inditex/webapi/controller/TestComercioElectronicoApplication.java
+++ comercioelectronico_insfrastructure/comercioelectronico_webapi_adapter/src/test/java/com/gft/inditex/webapi/controller/TestComercioElectronicoApplication.java
@@ -1,11 +1,11 @@
-package com.gft.inditex.webapi.boot;
+package com.gft.inditex.webapi.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude=SecurityAutoConfiguration.class)
-public class TestComercioElectronicoApplication{
+class TestComercioElectronicoApplication{
 
 	 public static void main(String[] args) {
 	        SpringApplication.run(TestComercioElectronicoApplication.class, args);
